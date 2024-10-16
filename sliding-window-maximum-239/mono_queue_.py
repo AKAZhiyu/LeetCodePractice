a6187c4c8c61cc9e9ba6149f649e75fa379f673b
AKAZhiyu <zhixuan.z2003@gmail.com>
2024-10-16 14:15:36 +0800

    Leetcode 239 is solved

---
+++ sliding-window-maximum-239/mono_queue_.py
@@ -0,0 +1,41 @@
+from collections import deque
+from typing import List
+
+class MonoDeque(deque):
+
+
+    # left-in right-out
+    # from small to big
+    def __init__(self):
+        super().__init__()
+
+    def pop(self, x):
+        if x == self[-1]:
+            super().pop()
+
+    def push(self, x):
+        while self and x > self[0]:
+            self.popleft()
+        self.appendleft(x)
+
+    def front(self):
+        return self[-1]
+
+class Solution:
+
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        result = []
+        mono_queue = MonoDeque()
+
+        for i in range(k): # 0 k-1
+            mono_queue.push(nums[i])
+        result.append(mono_queue.front())
+
+        for i in range(k, len(nums)): # k n-1
+            mono_queue.push(nums[i])
+            mono_queue.pop(nums[i - k])
+            result.append(mono_queue.front())
+
+        return result
+
+
