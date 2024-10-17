d8550f602372a02723c55741088c3c37371af647
AKAZhiyu <zhixuan.z2003@gmail.com>
2024-10-17 10:33:26 +0800

    Leetcode 94-144-145 is solved in iterative method

--- binary-tree-traversal-144-145-94/TreeNode.py
+++ binary-tree-traversal-144-145-94/TreeNode.py
@@ -1,52 +1,7 @@
-from typing import Optional, List
-
-
 class TreeNode:
     def __init__(self, val, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
-
-
-class Solution:
-    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        if root is None:
-            return []
-
-        result = []
-
-        result.append(root.val)
-        result.extend(self.preorderTraversal(root.left))
-        result.extend(self.preorderTraversal(root.right))
-
-        return result
-
-
-    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        if root is None:
-            return []
-
-        result = []
-
-        result.extend(self.postorderTraversal(root.left))
-        result.extend(self.postorderTraversal(root.right))
-        result.append(root.val)
-
-        return result
-
-    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        if root is None:
-            return []
-
-        result = []
-
-        result.extend(self.inorderTraversal(root.left))
-        result.append(root.val)
-        result.extend(self.inorderTraversal(root.right))
-
-        return result
-
-
-
 
 
---
+++ binary-tree-traversal-144-145-94/iterative-traversal/solution__.py
@@ -0,0 +1,70 @@
+from typing import Optional, List
+
+class TreeNode:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if root is None:
+            return []
+
+        stack = [root]
+        result = []
+        # mid left right
+        while stack:
+            processing = stack.pop()
+            result.append(processing.val)
+            if processing.right:
+                stack.append(processing.right)
+            if processing.left:
+                stack.append(processing.left)
+
+        return result
+
+
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if root is None:
+            return []
+
+        stack = []
+        result = []
+        stack.append(root)
+
+        # mid right left
+        while stack:
+            processing = stack.pop()
+            result.append(processing.val)
+            if processing.left:
+                stack.append(processing.left)
+            if processing.right:
+                stack.append(processing.right)
+
+
+        return result[::-1]
+
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        if not root:
+            return []
+        stack = []
+        result = []
+
+        curr = root
+
+        while curr or stack:
+            if curr is not None:
+                stack.append(curr)
+                curr = curr.left
+            else:
+                curr = stack.pop()
+                result.append(curr.val)
+                curr = curr.right
+
+        return result
+
+
+
+
+
