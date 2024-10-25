3a9f15a5cd1732dbed418814fe2f00e9546c44c7
AKAZhiyu <zhixuan.z2003@gmail.com>
2024-10-25 14:02:22 +0800

    Leetcode 404 is solved in 2 methods

---
+++ sum-of-left-leaves-404/recursive___.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
+        if root is None:
+            return 0
+        if root.left is None and root.right is None:
+            return 0
+
+        left_value = self.sumOfLeftLeaves(root.left)
+        if root.left and root.left.left is None and root.left.right is None:
+            left_value = root.left.val
+
+        right_value = self.sumOfLeftLeaves(root.right)
+
+        return left_value + right_value
+
