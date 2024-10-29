480b3c0f2ad771340aa1f41e4249168058993932
AKAZhiyu <zhixuan.z2003@gmail.com>
2024-10-29 10:41:00 +0800

    Leetcode 98 is solved

---
+++ validate-binary-search-tree-98/iterative___.py
@@ -0,0 +1,30 @@
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        stack = []
+        pre = None
+        cur = root
+
+        while cur is not None or len(stack) > 0:
+            if cur is not None:
+                stack.append(cur)
+                cur = cur.left
+
+            else:
+                cur = stack.pop()
+                if pre is not None and pre.val >= cur.val:
+                    return False
+                pre = cur
+                cur = cur.right
+        return True
+
