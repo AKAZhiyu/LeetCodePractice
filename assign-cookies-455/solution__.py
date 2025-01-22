3bb58b4580b8212777d443560cd9f0c339c07edc
AKAZhiyu <zhixuan.z2003@gmail.com>
2025-01-22 11:42:21 +0700

    solve leetcode 455 using greedy algorithm

---
+++ assign-cookies-455/solution__.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        g.sort()
+        s.sort()
+        result = 0
+        bisc_index = len(s) - 1
+        for i in range(len(g) - 1, -1, -1):
+            if i >= 0 and g[i] <= s[bisc_index]:
+                bisc_index -= 1
+                result += 1
+        return result
