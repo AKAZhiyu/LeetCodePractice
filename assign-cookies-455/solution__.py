f9c4571b4f955f10c29bf52e3583366804182865
AKAZhiyu <zhixuan.z2003@gmail.com>
2025-01-22 11:47:30 +0700

    solve leetcode 455 using greedy algorithm

--- assign-cookies-455/solution__.py
+++ assign-cookies-455/solution__.py
@@ -8,7 +8,7 @@ class Solution:
         result = 0
         bisc_index = len(s) - 1
         for i in range(len(g) - 1, -1, -1):
-            if i >= 0 and g[i] <= s[bisc_index]:
+            if bisc_index >= 0 and g[i] <= s[bisc_index]:
                 bisc_index -= 1
                 result += 1
         return result
