854f11189e829e1711d322c91b9e58ab9bc3d63b
AKAZhiyu <zhixuan.z2003@gmail.com>
2025-02-09 00:06:20 +0800

    solve leetcode 56 using greedy

---
+++ merge-intervals-56/solution__.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        result = []
+        if not intervals:
+            return result
+        intervals.sort(key=lambda x: x[0])
+        result.append(intervals[0])
+        for i in range(1, len(intervals)):
+            if result[-1][1] >= intervals[i][0]:
+                result[-1][1] = max(result[-1][1], intervals[i][1])
+            else:
+                result.append(intervals[i])
+        return result
