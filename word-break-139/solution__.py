0e1bd265fa1f3cb19dff30ce30614e186c95b80c
AKAZhiyu <zhixuan.z2003@gmail.com>
2025-10-23 11:24:45 +0800

    solve leetcode 139 using dp

---
+++ word-break-139/solution__.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        n = len(s)
+        dp = [False] * (n + 1)
+        dp[0] = True
+
+        for i in range(1, n + 1):
+            for word in wordDict:
+                if len(word) > i:
+                    break
+                if dp[i - len(word)] and word == s[i - len(word): i]:
+                    dp[i] = True
+        return dp[-1]
+
+
