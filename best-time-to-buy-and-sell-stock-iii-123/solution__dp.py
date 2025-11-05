d800e61fa63a3492efa3a356ebe438d5a36947eb
AKAZhiyu <zhixuan.z2003@gmail.com>
2025-11-05 10:55:52 +0800

    solve leetcode 123 using dp

---
+++ best-time-to-buy-and-sell-stock-iii-123/solution__dp.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        n = len(prices)
+
+        dp = [[0] * 5 for i in range(n)]
+        dp[0][0] = 0
+        dp[0][1] = -prices[0]
+        dp[0][2] = 0
+        dp[0][3] = -prices[0]
+        dp[0][4] = 0
+
+        for i in range(1, n):
+            dp[i][1] = max(-prices[i], dp[i - 1][1])
+            dp[i][2] = max(dp[i - 1][1] + prices[i], dp[i - 1][2])
+            dp[i][3] = max(dp[i - 1][2] - prices[i], dp[i - 1][3])
+            dp[i][4] = max(dp[i - 1][3] + prices[i], dp[i - 1][4])
+        return dp[-1][-1]
