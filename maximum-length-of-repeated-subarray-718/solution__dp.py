c10919926a27cab36bff80844f4480232ee4c6ab
AKAZhiyu <zhixuan.z2003@gmail.com>
2025-11-18 09:59:32 +0800

    solve leetcode 718 using dp 1-d

--- maximum-length-of-repeated-subarray-718/solution__dp.py
+++ maximum-length-of-repeated-subarray-718/solution__dp.py
@@ -2,19 +2,35 @@ from typing import List
 
 
 class Solution:
+    # def findLength(self, nums1: List[int], nums2: List[int]) -> int:
+        # n, m = len(nums1), len(nums2)
+        #
+        # dp = [[0] * (m + 1) for _ in range(n + 1)]
+        #
+        # result = 0
+        #
+        # for i in range(1, n + 1):
+        #     for j in range(1, m + 1):
+        #         if nums1[i - 1] == nums2[j - 1]:
+        #             dp[i][j] = dp[i - 1][j - 1] + 1
+        #             result = max(result, dp[i][j])
+        # return result
     def findLength(self, nums1: List[int], nums2: List[int]) -> int:
         n, m = len(nums1), len(nums2)
-
-        dp = [[0] * (m + 1) for _ in range(n + 1)]
-
+        dp = [0] * (m + 1)
         result = 0
-
         for i in range(1, n + 1):
-            for j in range(1, m + 1):
+            for j in range(m, 0, -1):
                 if nums1[i - 1] == nums2[j - 1]:
-                    dp[i][j] = dp[i - 1][j - 1] + 1
-                    result = max(result, dp[i][j])
+                    dp[j] = dp[j - 1] + 1
+
+                else:
+                    dp[j] = 0
+                result = max(result, dp[j])
         return result
+
+
+
 
 
 
