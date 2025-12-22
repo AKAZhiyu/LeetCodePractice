ed55bf205bff12861488ed98409fe74fc835d4c9
AKAZhiyu <zhixuan.z2003@gmail.com>
2025-12-22 11:22:02 +0800

    finish leetcode 647 using dp

---
+++ palindromic-substrings-647/solution___.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def countSubstrings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        n = len(s)
+
+        dp = [[False] * n for _ in range(n)]
+        result = 0
+        for i in range(n - 1, -1, -1):
+            for j in range(i, n):
+                if s[i] == s[j]:
+                    if j - i < 2:
+                        result += 1
+                        dp[i][j] = True
+                    elif dp[i + 1][j - 1]:
+                        result += 1
+                        dp[i][j] = True
+        return result
+
