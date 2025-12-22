996f6d4385485de2ddbcbfa345f0bf2469a9d26c
AKAZhiyu <zhixuan.z2003@gmail.com>
2025-12-22 11:59:44 +0800

    solve leetcode 516 using dp

---
+++ longest-palindromic-subsequence-516/solution__dp.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def longestPalindromeSubseq(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        n = len(s)
+
+        dp = [[0] * n for _ in range(n)]
+
+        for i in range(n):
+            dp[i][i] = 1
+
+        for i in range(n-1, -1, -1):
+            for j in range(i+1, n):
+                if s[i] == s[j]:
+                    dp[i][j] = 2 + dp[i + 1][j - 1]
+                else:
+                    dp[i][j] = max(dp[i + 1][j], dp[i][j - 1])
+
+        return dp[0][-1]
+
+
