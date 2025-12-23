41b3c7ca175c5396a60c5a566745b4dae3290895
AKAZhiyu <zhixuan.z2003@gmail.com>
2025-12-23 22:32:40 +0800

    solve leetcode 496 using stack

---
+++ next-greater-element-i-496/solution_stack.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        result = [-1] * len(nums1)
+
+        stack = [0]
+        for i in range(1, len(nums2)):
+            while stack and nums2[stack[-1]] < nums2[i]:
+                if nums2[stack[-1]] in nums1:
+                    idx = nums1.index(nums2[stack[-1]])
+                    result[idx] = nums2[i]
+
+                stack.pop()
+            stack.append(i)
+        return result
+
